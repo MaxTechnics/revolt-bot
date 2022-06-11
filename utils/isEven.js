044d1b5ee430c6a02ebd9066d79972f6ce6cecae
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2022-06-11 16:27:45 +0200

    More removed shit

--- utils/isEven.js
+++
@@ -1,9 +0,0 @@
-const isEven = (n) => {
-	if (!n && n !== 0) throw new Error('No param geven');
-	if (isNaN(n)) throw new Error('Parameter is not a number');
-	return n % 2 === 0 ? true : false;
-};
-
-module.exports = {
-	isEven
-};
